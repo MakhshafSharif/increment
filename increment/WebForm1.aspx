9fc892611ad5502f287f85b10ba4572777a137fc
MakhshafSharif <108417493+MakhshafSharif@users.noreply.github.com>
2022-07-26 00:25:03 +0500

    Add files via upload

---
+++ increment/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="increment.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <asp:TextBox ID="TextBox1" runat="server" EnableViewState="true" ></asp:TextBox>  
+            <asp:Label ID="label1" runat="server" Text="0"></asp:Label>  
+            <asp:Button ID="Button1" runat="server" OnClick="IncrementCounter" Text="Submit"  /> 
+        </div>
+    </form>
+</body>
+</html>
---
+++ increment/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace increment
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                ViewState["Count"] = 0;
+            }
+        }
+        protected void IncrementCounter(object sender, EventArgs e)
+        {
+            int counter;
+            if (ViewState["Count"] != null)
+            {
+                counter = Convert.ToInt32(ViewState["Count"]);
+            }
+            else
+            {
+                counter = 0;
+            }
+            counter = counter + 1;
+            ViewState["Count"] = counter;
+            this.label1.Text = counter.ToString();
+        }
+    }
+}
